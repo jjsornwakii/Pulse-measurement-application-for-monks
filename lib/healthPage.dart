a6059a52247dd2d73839fd84aef4d0f5bc9c46a1
s6404062630317 <s6404062630317@email.kmutnb.ac.th>
2024-06-19 17:47:04 +0700

    Nav &&  healPage
    healthPage ----->เอาวางใหม่ทั้งหมดเลย
    navigation ----> เเก้ ส่งส่งหน้า

--- lib/healthPage.dart
+++ lib/healthPage.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
-import 'package:sato/bloodpresure.dart';
-import 'package:sato/heartrate.dart';
 import 'dart:convert';
 
-import 'package:sato/sugarblood.dart';
+import 'bloodpresure.dart';
+import 'heartrate.dart';
+import 'sugarblood.dart';
 
 class HealthPage extends StatefulWidget {
   @override
-  _HealthPage createState() => _HealthPage();
+  _HealthPageState createState() => _HealthPageState();
 }
 
-class _HealthPage extends State<HealthPage> {
+class _HealthPageState extends State<HealthPage> {
   final GetStorage box = GetStorage();
   final server = dotenv.env['server'] ?? '';
   final port = dotenv.env['port'] ?? '';
@@ -21,11 +21,13 @@ class _HealthPage extends State<HealthPage> {
   Map<String, dynamic> userData = {};
 
   @override
+  void initState() {
+    super.initState();
+    _getUserData();
+  }
+
   Future<void> _getUserData() async {
-    final server = 'your_server_address'; // Replace with your server address
-    final port = 'your_server_port'; // Replace with your server port
-    final apipath = 'your_api_path'; // Replace with your API endpoint path
-    final url = 'http://$server:$port/$apipath/userinfo.php';
+    final url = 'http://$server:$port/$apipath/getHealthData.php';
     final response = await http.post(
       Uri.parse(url),
       headers: <String, String>{
@@ -46,193 +48,113 @@ class _HealthPage extends State<HealthPage> {
     }
   }
 
-  Widget build(BuildContext context) {
-    double _sliderValue = 0.5;
-    double fontSize = 17;
+  Widget buildInfoCard(String title, String value, String unit,
+      String imagePath, Widget targetPage) {
     final screenSize = MediaQuery.of(context).size;
-    return Container(
-      width: screenSize.width,
-      height: screenSize.height,
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 255, 251, 138),
+    double fontSize = 17;
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 255, 217, 29),
+        minimumSize: Size(screenSize.width * .95, screenSize.height * .15),
+        maximumSize: Size(screenSize.width * .95, screenSize.height * .15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
       ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 15,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 255, 217, 29),
-                minimumSize:
-                    Size(screenSize.width * .95, screenSize.height * .15),
-                maximumSize:
-                    Size(screenSize.width * .95, screenSize.height * .15),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20))),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HeartRatePage()),
-              );
-            },
-            child: Column(
-              children: [
-                Text(
-                  'แตะเพื่อวัด',
-                  style: TextStyle(
-                    fontSize: fontSize,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 50, 52, 62),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset(
-                          'assets/icon/blood_glucose.png', // ใส่ path ของรูปภาพที่ต้องการใช้
-                          height: 50,
-                        ),
-                        Text(
-                          "อัตตราการเต้นของหัวใจ",
-                          style: TextStyle(
-                            fontSize: fontSize,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                  ],
-                ),
-              ],
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => targetPage),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Image.asset(
+              imagePath,
+              height: 50,
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 255, 217, 29),
-                minimumSize:
-                    Size(screenSize.width * .95, screenSize.height * .15),
-                maximumSize:
-                    Size(screenSize.width * .95, screenSize.height * .15),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20))),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => BloodPressurePage()),
-              );
-            },
-            child: Column(
-              children: [
-                Text(
-                  'แตะเพื่อวัด',
-                  style: TextStyle(
-                    fontSize: fontSize,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 50, 52, 62),
+            SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: fontSize,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 50, 52, 62),
+                    ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset(
-                          'assets/icon/blood_glucose.png', // ใส่ path ของรูปภาพที่ต้องการใช้
-                          height: 50,
-                        ),
-                        Text(
-                          "ความดันโลหิต",
-                          style: TextStyle(
-                            fontSize: fontSize,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
+                  Text(
+                    '$value $unit',
+                    style: TextStyle(
+                      fontSize: fontSize,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 255, 217, 29),
-                minimumSize:
-                    Size(screenSize.width * .95, screenSize.height * .15),
-                maximumSize:
-                    Size(screenSize.width * .95, screenSize.height * .15),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20))),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SugarbloodPage()),
-              );
-            },
-            child: Column(
-              children: [
-                Text(
-                  'แตะเพื่อวัด',
-                  style: TextStyle(
-                    fontSize: fontSize,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 50, 52, 62),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset(
-                          'assets/icon/blood_glucose.png', // ใส่ path ของรูปภาพที่ต้องการใช้
-                          height: 50,
-                        ),
-                        Text(
-                          "ระดับน้ำตาลในเลือด",
-                          style: TextStyle(
-                            fontSize: fontSize,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
+                  Text(
+                    'แตะเพื่อดูรายละเอียดเพิ่มเติม',
+                    style: TextStyle(
+                      fontSize: fontSize * 0.8,
+                      color: Colors.black,
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Icon(Icons.info, color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: screenSize.width,
+        height: screenSize.height,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 255, 254, 216),
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: 15),
+            if (userData.isNotEmpty) ...[
+              buildInfoCard(
+                'ความดันโลหิต',
+                '${userData['blood_pressure_min']} / ${userData['blood_pressure_max']}',
+                'mmHg',
+                'assets/icon/blood_glucose.png',
+                BloodPressurePage(),
+              ),
+              SizedBox(height: 10),
+              buildInfoCard(
+                'ระดับน้ำตาลในเลือด',
+                '${userData['blood_sugar']}',
+                'mg/dL',
+                'assets/icon/blood_glucose.png',
+                SugarbloodPage(),
+              ),
+              SizedBox(height: 10),
+              buildInfoCard(
+                'อัตราการเต้นของหัวใจ',
+                '${userData['heart_rate']}',
+                'bpm',
+                'assets/icon/blood_glucose.png',
+                HeartRatePage(),
+              ),
+            ] else ...[
+              CircularProgressIndicator(), // Show a loading indicator while fetching data
+            ],
+          ],
+        ),
       ),
     );
   }
